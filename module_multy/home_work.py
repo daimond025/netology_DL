57c8ffca8ca09b0b63c94024b94e85c7f98573f9
daimond025 <daimond025@yandex.ru>
2024-11-28 07:28:54 +0300

    Вторая домашка

--- module_multy/home_work.py
+++ module_multy/home_work.py
@@ -1,16 +1,114 @@
+import time
+
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torchvision as tv
 
-
+num_epochs = 10
 BATCH_SIZE=256
 train_dataset = tv.datasets.MNIST('.', train=True, transform=tv.transforms.ToTensor(), download=True)
 test_dataset = tv.datasets.MNIST('.', train=False, transform=tv.transforms.ToTensor(), download=True)
 train = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE)
 test = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
-print(train_dataset)
+
+model = torch.nn.Sequential(
+    torch.nn.Flatten(),
+    torch.nn.Linear(784, 256),
+    torch.nn.ReLU(),
+    torch.nn.Linear(256, 10)
+)
+
+model = torch.nn.Sequential(
+    torch.nn.Flatten(),
+    torch.nn.Linear(784, 512),
+    torch.nn.ReLU(),
+    torch.nn.Linear(512, 420),
+    torch.nn.ReLU(),
+    torch.nn.Linear(420, 256),
+    torch.nn.ReLU(),
+    torch.nn.Linear(256, 128),
+    torch.nn.ReLU(),
+    torch.nn.Linear(128, 10)
+)
+
+model = torch.nn.Sequential(
+    torch.nn.Flatten(),
+    torch.nn.Linear(784, 512),
+    torch.nn.ReLU(),
+    torch.nn.BatchNorm1d(512),
+    torch.nn.Linear(512, 420),
+    torch.nn.ReLU(),
+    torch.nn.BatchNorm1d(420),
+    torch.nn.Linear(420, 256),
+    torch.nn.ReLU(),
+    torch.nn.BatchNorm1d(256),
+    torch.nn.Linear(256, 128),
+    torch.nn.ReLU(),
+    torch.nn.BatchNorm1d(128),
+    torch.nn.Linear(128, 10)
+)
+
+model = torch.nn.Sequential(
+    torch.nn.Flatten(),
+    torch.nn.Linear(784, 420),
+    torch.nn.ReLU(),
+    torch.nn.BatchNorm1d(420),
+    torch.nn.Dropout(0.2),
+
+    torch.nn.Linear(420, 256),
+    torch.nn.ReLU(),
+    torch.nn.BatchNorm1d(256),
+    torch.nn.Dropout(0.2),
+
+    torch.nn.Linear(256, 128),
+    torch.nn.ReLU(),
+    torch.nn.BatchNorm1d(128),
+    torch.nn.Dropout(0.2),
+
+    torch.nn.Linear(128, 10)
+)
+
+loss = torch.nn.CrossEntropyLoss()
+trainer = torch.optim.SGD(model.parameters(), lr=.01)
+trainer = torch.optim.Adam(model.parameters(), lr=.01, weight_decay=.01)
 
 
+def train_model():
+    for ep in range(num_epochs):
+        train_iters, train_passed = 0, 0
+        train_loss, train_acc = 0., 0.
+        start = time.time()
+
+        model.train()
+        for X, y in train:
+            trainer.zero_grad()
+            y_pred = model(X)
+            l = loss(y_pred, y)
+            l.backward()
+            trainer.step()
+
+            train_loss += l.item()
+            train_acc += (y_pred.argmax(dim=1) == y).sum().item()
+            train_iters += 1
+            train_passed += len(X)
+
+        test_iters, test_passed = 0, 0
+        test_loss, test_acc = 0., 0.
+        model.eval()
+        for X, y in test:
+            y_pred = model(X)
+            l = loss(y_pred, y)
+            test_loss += l.item()
+            test_acc += (y_pred.argmax(dim=1) == y).sum().item()
+            test_iters += 1
+            test_passed += len(X)
+
+        print("ep: {}, taked: {:.3f}, train_loss: {}, train_acc: {}, test_loss: {}, test_acc: {}".format(
+            ep, time.time() - start, train_loss / train_iters, train_acc / train_passed,
+                test_loss / test_iters, test_acc / test_passed)
+        )
+
+train_model()
