8586b017a8c3faf35dd2cbf346c9898b9b89d6cf
daimond025 <daimond025@yandex.ru>
2025-02-13 22:54:17 +0300

    Домашка тест 1

--- text1/clasifer.py
+++ text1/clasifer.py
@@ -14,48 +14,39 @@ import nltk
 import pandas as pd
 
 data = pd.read_csv('data/simpsons_script_lines.csv', on_bad_lines='skip')
+data.dropna(subset=["normalized_text" ], inplace=True)
+
 data = data[(data['raw_character_text'] =='Lisa Simpson') | (data['raw_character_text'] =='Bart Simpson')]
 
 data_bart = data[(data['raw_character_text'] =='Bart Simpson')]
 data_bart_text = list(data_bart['normalized_text'].values)
+
+data_lisa = data[(data['raw_character_text'] =='Lisa Simpson')]
+data_lisa_text = list(data_bart['normalized_text'].values)
+
 w2v_model = Word2Vec.load("word2vec.model")
 
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-text = ["She sells seashells by the seashore","The sea.","The seashore"]
-vectorizer = TfidfVectorizer()
-vectorizer.fit(data_bart_text)
-print(vectorizer)
-exit()
 
 def get_phrase_embedding(model, secs):
     collect = []
     for sec in secs:
-        collect = []
-        text_vector = model.wv.get_mean_vector(sec)
+        collect_sec = model.wv.get_mean_vector(sec)
+        # collect_sec = []
+        # for word in sec:
+        #     if word in  model.wv.key_to_index.keys():
+        #         index_word = model.wv.key_to_index[word]
+        #         vector = np.array(model.wv[index_word])
+        #         collect_sec.append(vector)
+        #
+        # collect_sec = np.mean(collect_sec, axis=0)
+        # collect_sec = collect_sec / np.linalg.norm(collect_sec)
+        collect.append(collect_sec)
 
-        for word in sec:
-            if word in  model.wv.key_to_index.keys():
-                index_word = model.wv.key_to_index[word]
-
-                vector = np.array(model.wv[index_word])
-                collect.append(vector)
-
-        ret = np.mean(np.array(collect), axis=0)
-        ret = ret / np.linalg.norm(ret)
-        print(sec)
-        print(ret[:50])
-        print(text_vector[:50])
-        exit()
-
-    print(ret.shape)
-    exit()
+    return collect
+data_bart_text = get_phrase_embedding(w2v_model, data_bart_text)
+data_bart_text = get_phrase_embedding(w2v_model, data_lisa_text)
 
 
-
-    vector = np.zeros([model.vector_size], dtype='float32')
-
-    return vector
-get_phrase_embedding(w2v_model, data_bart_text)
+print(len(data_bart_text))
+exit()
 
